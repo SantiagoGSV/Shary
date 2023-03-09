1154b4bef816edeab3313f887f5afd0918f7b4de
Santiago Guadarrama <santiagogsv@icloud.com>
2023-03-09 15:47:19 -0600

    Initial Commit

---
+++ Shary/SharyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharyApp.swift
+//  Shary
+//
+//  Created by Santiago Guadarrama on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SharyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
